b5403f0416dc534ecb10673a101e1ea0b5cd9666
FlailoftheLord <jeddyh15@gmail.com>
2019-06-05 22:55:17 -0400

    Initial commit

---
+++ src/me/flail/nou/NoU.java
@@ -0,0 +1,94 @@
+package me.flail.nou;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.flail.slashplayer.DataFile;
+import me.flail.slashplayer.tools.Logger;
+
+public class NoU extends JavaPlugin implements Listener {
+
+	public Server server;
+	public DataFile settings;
+	private Logger tools;
+	public List<String> adj = new ArrayList<>(8);
+
+	@Override
+	public void onLoad() {
+		tools = new Logger();
+
+		server = this.getServer();
+
+		new Settings(this).load();
+	}
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getCommand("nou").setExecutor(this);
+
+		adj.add("you're");
+		adj.add("youre");
+		adj.add("you");
+		adj.add("ur");
+		adj.add("you are");
+		adj.add("u r");
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("nou")) {
+			if (sender.hasPermission(settings.getValue("CommandPermission"))) {
+				new Settings(this).load();
+
+				sender.sendMessage(tools
+						.chat(settings.getValue("Color") + "No U &7v" + getDescription().getVersion() + " &2by FlailoftheLord."));
+				sender.sendMessage(tools.chat("&aUpdated Settings!"));
+			}
+		}
+		return true;
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void playerSayNoU(AsyncPlayerChatEvent event) {
+		String message = event.getMessage();
+		Player player = event.getPlayer();
+
+		for (String phrase : adj) {
+			if (tools.msgCheck(message, phrase, "contains")) {
+				String nou = tools.chat(settings.getValue("Color") + "no u");
+
+				if (player.hasPermission(settings.getValue("Permission"))
+						&& !player.hasPermission(settings.getValue("ExemptPermission"))) {
+
+					server.getScheduler().scheduleSyncDelayedTask(this, () -> {
+						player.sendMessage(nou);
+
+						if (settings.getBoolean("LogChat")) {
+							String log = "Player: " + player.getName() + ". Said \"" + message + "\". And was told no u";
+
+							tools.log(log);
+						}
+
+					}, 10L);
+
+				}
+
+				return;
+			}
+		}
+
+	}
+
+}
---
+++ src/me/flail/nou/Settings.java
@@ -0,0 +1,58 @@
+package me.flail.nou;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import me.flail.slashplayer.DataFile;
+import me.flail.slashplayer.tools.Logger;
+
+public class Settings extends Logger {
+
+	private DataFile settings;
+
+	public Settings(NoU plugin) {
+		this.plugin = plugin;
+		settings = new DataFile("Settings.yml");
+	}
+
+	public void load() {
+
+		settings.setHeader("#-----------------------------------------------------------------\r\n" +
+				"#==================================================================#\r\n" +
+				"#                                                                  #\r\n" +
+				"#                   NoU by FlailoftheLord.                         #\r\n" +
+				"#         -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-                  #\r\n" +
+				"#           If you have any Questions or feedback                  #\r\n" +
+				"#              Join my discord server here:                        #\r\n" +
+				"#               https://discord.gg/wuxW5PS                         #\r\n" +
+				"#   ______               __        _____                           #\r\n" +
+				"#   |       |           /  \\         |        |                    #\r\n" +
+				"#   |__     |          /____\\        |        |                    #\r\n" +
+				"#   |       |         /      \\       |        |                    #\r\n" +
+				"#   |       |_____   /        \\    __|__      |______              #\r\n" +
+				"#                                                                  #\r\n" +
+				"#==================================================================#\r\n" +
+				"#-----------------------------------------------------------------\r\n\n");
+
+		Map<String, Object> values = new HashMap<>();
+		values.put("Color", "&c");
+		values.put("Permission", "nou.chat");
+		values.put("CommandPermission", "nou.command");
+		values.put("ExemptPermission", "nou.exempt");
+		values.put("LogChat", Boolean.valueOf(true));
+
+		for (String key : values.keySet()) {
+			if (!settings.hasValue(key)) {
+				settings.setValue(key, values.get(key));
+			}
+		}
+
+
+		plugin.settings = settings;
+	}
+
+	public String get(String key) {
+		return settings.hasValue(key) ? settings.getValue(key) : "Invalid value: " + key;
+	}
+
+}
